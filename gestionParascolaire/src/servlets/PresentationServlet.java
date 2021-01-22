39741c55b7b184b7878f10d335316b6b5eac8f9c
amineharka <59688444+amineharka@users.noreply.github.com>
2021-01-22 11:01:28 +0100

    another push

--- gestionParascolaire/src/servlets/PresentationServlet.java
+++ gestionParascolaire/src/servlets/PresentationServlet.java
@@ -7,12 +7,8 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-import org.hibernate.SessionFactory;
-
 import beans.Club;
-import dao.DaoClub;
 import dao.DaoClubImpl;
-import dao.HibernateUtil;
 
 /**
  * Servlet implementation class PresentationServlet
@@ -21,42 +17,28 @@ import dao.HibernateUtil;
 public class PresentationServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    private DaoClub daoClub;
+    /**
+     * @see HttpServlet#HttpServlet()
+     */
     public PresentationServlet() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-    
-	public void init() throws ServletException{
-		
-		SessionFactory factory = HibernateUtil.getSessionFactory();
-		this.daoClub = new DaoClubImpl(factory);
-		
-	}
-    
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		// TODO Auto-generated method stub
 		response.getWriter().append("Served at: ").append(request.getContextPath());
 	}
 
-	
-	
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		
 		
-<<<<<<< HEAD
-=======
-		
-		String name=request.getParameter("name");
-		Club club = daoClub.get(name);
-		
-		System.out.println(club.getId());
-		System.out.println(club.getName());
-		System.out.println(club.getDescription());
-		System.out.println(club.getParagraphe());
-		
->>>>>>> ffd4dc4d9f00ed13a0863bf0c76ebfeea08cd1ab
 	}
 
 }
