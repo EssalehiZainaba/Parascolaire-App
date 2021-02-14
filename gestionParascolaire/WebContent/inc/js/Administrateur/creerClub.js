380564e30a0467fea608f2893100c1d527ffdd1f
Kellouch <younes.kellouch@gmail.com>
2021-02-14 21:06:27 +0100

    Fix the form (in case errors ) + positioning form +"sticky"+removing useless js Code

--- gestionParascolaire/WebContent/inc/js/Administrateur/creerClub.js
+++ gestionParascolaire/WebContent/inc/js/Administrateur/creerClub.js
@@ -1,17 +1,8 @@
 const clubsContainer=document.querySelector("#clubsContainer");
 const creerButton=document.querySelector("#creerButton");
 const addClubForm=document.querySelector("#addClub");
-const container=document.querySelector(".container");
 const closeAddForm=document.querySelector("#addClub .close");
-const addClubButton=document.querySelector("#submitButton");
 
-clubsContainer.addEventListener("click",(event)=>{
-    
-    if(event.target.tagName==="I"){
-        let removeClub=event.target.parentElement;
-        clubsContainer.removeChild(removeClub);
-    }
-});
 
 creerButton.addEventListener("click",()=>{
     addClubForm.style.transform="translateY(0)";
@@ -25,16 +16,4 @@ closeAddForm.addEventListener("click",()=>{
     clubsContainer.style.pointerEvents="";
 })
 
-addClubButton.addEventListener("click",()=>{
-    const text=document.createTextNode(document.querySelector("#addClub input").value);
-    const nouveauClub=document.createElement("div");
-    nouveauClub.className="club";
-    const clubName=document.createElement("p");
-    clubName.appendChild(text);
-    const deleteIcon=document.createElement("i");
-    deleteIcon.className="fas fa-trash";
-    nouveauClub.appendChild(clubName);
-    nouveauClub.appendChild(deleteIcon);
-    clubsContainer.insertBefore(nouveauClub,document.querySelector("#creerButton"));  
 
-})
